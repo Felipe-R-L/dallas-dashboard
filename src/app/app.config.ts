9f79931b29f604cbeb4c528e66ace0c9cd2e8f5a
Felipe-R-L <leone.feliper@gmail.com>
2025-09-28 14:18:54 -0300

    fix: add script to build env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,14 +9,7 @@ import { provideRouter } from '@angular/router';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-const firebaseConfig = {
-  apiKey: process.env['apiKey'],
-  authDomain: process.env['authDomain'],
-  projectId: process.env['projectId'],
-  storageBucket: process.env['storageBucket'],
-  messagingSenderId: process.env['messagingSenderId'],
-  appId: process.env['appId'],
-};
+import { environment } from '../environments/environment.prod';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,7 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
   ],
