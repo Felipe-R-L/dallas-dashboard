01a3264510061b3fea87bf45e7bff6715d4ae03a
Felipe-R-L <leone.feliper@gmail.com>
2025-09-28 19:57:31 -0300

    fix: definitive fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA1NlXGW4ntwH6BGRz3XI3qQeCrE7IUrJg',
+    authDomain: 'dallas-dashboard-f9475.firebaseapp.com',
+    projectId: 'dallas-dashboard-f9475',
+    storageBucket: 'dallas-dashboard-f9475.firebasestorage.app',
+    messagingSenderId: '1063989432585',
+    appId: '1:1063989432585:web:1d2bf0627a25b6a376c3d8',
+  },
+};
